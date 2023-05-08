6903cd347b98e13f50092790dd4c571973cdafc0
Takis Stamatopoulos <124575928+ChainsawPerson@users.noreply.github.com>
2023-05-08 14:25:15 +0300

    Added School Table

--- Create Libary DB.sql
+++ Create Libary DB.sql
@@ -1,3 +1,17 @@
 DROP SCHEMA IF EXISTS libary;
 CREATE SCHEMA libary;
 USE libary;
+
+CREATE TABLE School (
+  school_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  school_admin_id INT UNSIGNED,
+  school_name VARCHAR(50) NOT NULL,
+  school_mail_address VARCHAR(70) NOT NULL,
+  city VARCHAR(40) NOT NULL,
+  school_phone_number VARCHAR(10) NOT NULL,
+  school_email VARCHAR(50) NOT NULL,
+  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (school_id),
+  KEY idx_school_admin_id (school_admin_id)
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  
