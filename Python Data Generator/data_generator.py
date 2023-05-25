54ad9ab8dc4dcf7298399f0b60b0850263fc41e1
StavrosLzp <73658190+StavrosLzp@users.noreply.github.com>
2023-05-25 08:24:30 -0700

    generator

--- Python Data Generator/data_generator.py
+++ Python Data Generator/data_generator.py
@@ -2,6 +2,7 @@ import faker
 from datetime import datetime
 from collections import OrderedDict
 import os
+from faker.providers import isbn
 
 path = os.getcwd() + "/Python Data Generator/"
 
@@ -12,13 +13,29 @@ fake = faker.Faker()
 
 DUMMY_DATA_NUMBER = 20
 table_name = "author"
-table_columns = ["author_first_name", "author_last_name",]
+table_columns = ["author_first_name", "author_last_name"]
 content = ""
 
 for i in range(DUMMY_DATA_NUMBER):
     firstName = fake.first_name()
     lastName = fake.last_name()
-    content += f'INSERT INTO {table_name} ({",".join(table_columns)}) VALUES ("{firstName}", "{lastName}");\n'
+    content += f'INSERT INTO {table_name} ({",".join(table_columns)})\
+        VALUES ("{firstName}", "{lastName}");\n'
+
+
+
+# Book
+
+DUMMY_DATA_NUMBER = 100
+table_name = "book"
+table_columns = ["book_ISBN", "book_title","book_page_no","book_subject","book_language",]
+content = ""
+
+for i in range(DUMMY_DATA_NUMBER):
+    isbn = fake.isbn10()
+    title = fake
+    content += f'INSERT INTO {table_name} ({",".join(table_columns)})\
+        VALUES ("{isbn}", "{lastName}");\n'
 
 
 
