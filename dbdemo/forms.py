d73cc4b9837b3839bce82f66281045f70e86e4ae
StavrosLzp <stavros.lazopoulos@gmail.com>
2023-05-28 12:26:01 +0300

    forms

---
+++ dbdemo/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, DateField, TextAreaField, SelectField, RadioField
+from wtforms.validators import DataRequired, Email, Optional, NumberRange
+
+# when passed as a parameter to a template, an object of this class will be rendered as a regular HTML form
+# with the additional restrictions specified for each field
+
+
+class login(FlaskForm):
+    username = StringField(label = "Username", validators = [DataRequired(message = "Username is a required field.")])
+
+    password = StringField(label = "Password", validators = [DataRequired(message = "Password is a required field.")])
